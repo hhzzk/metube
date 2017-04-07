32ed0cf1f61c073d00a863a20f59b5f6f54a9ebd
Jinrui Wang <wangjinrui2013@gmail.com>
2017-04-07 17:40:06 -0400

    add subscriptions.php

---
+++ database/tb_subscription.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once("db_conn.php");
+
+function add_subscription($infos)
+{
+    if(count($infos) != 6)
+    {
+        return -1;
+    }
+
+    $sql = "INSERT
+            INTO media 
+            (media_id, media_name, upload_time, size, category, user_name) 
+            VALUES 
+            $infos[media_id], $infos[media_name], $infos[upload_time], 
+            $infos[size], $infos[category],$infos[username]";
+    if(db_query($sql))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function get_subscriptions($user_id)
+{
+    $sql = "SELECT *
+            FROM subscription 
+            WHERE user_id=$user_id";    
+
+    $rows= db_select($sql);
+    if($rows== false)
+    {
+        return false;
+    }
+    else
+    {
+       return $rows;  
+    }
+}
+
+?>
