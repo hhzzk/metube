f143a34a95ddc962c875a4c482d1796c05feb54d
Jinrui Wang <wangjinrui2013@gmail.com>
2017-04-08 19:42:29 -0400

    add user profile

---
+++ person/profile.php
@@ -0,0 +1,65 @@
+
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <div class="main-grids">
+	    <div class="top-grids">
+	        <div class="recommended-info">
+	            <h3>Profile</h3>
+            </div>
+
+<form class="form-horizontal">
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="user_id">User ID:</label>
+    <div class="col-sm-6">
+      <input type="email" class="form-control" id="user_id" placeholder="User ID">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="user_name">User Name:</label>
+    <div class="col-sm-6">
+      <input type="email" class="form-control" id="user_name" placeholder="User Name">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="email">Email:</label>
+    <div class="col-sm-6">
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="phone">Phone:</label>
+    <div class="col-sm-6">
+      <input type="email" class="form-control" id="phone" placeholder="Enter Phone">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Password:</label>
+    <div class="col-sm-6"> 
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd1">Password:</label>
+    <div class="col-sm-6"> 
+      <input type="password" class="form-control" id="pwd1" placeholder="Enter password again">
+    </div>
+  </div>
+  <div class="form-group"> 
+    <div class="col-sm-offset-2 col-sm-6">
+      <button type="submit" class="btn btn-default">Submit</button>
+    </div>
+  </div>
+</form>
+
+
+		</div>
+
+		</div>
+	</div>
+
+			<!-- footer -->
+<?php
+    include("./footer.php");
+?>
+
+			<!-- //footer -->
+</div>
