5de93f320f9555fedcc86bb6e56db4f08a16c82c
Jinrui Wang <wangjinrui2013@gmail.com>
2017-04-18 23:45:48 -0400

    Update group.php

--- user/group.php
+++ user/group.php
@@ -7,37 +7,27 @@
 
 <div class="container">
             <div class="col-sm-12">
+		    
+<div class="alert alert-success" role="alert">
+  <strong>Well done!</strong> You successfully read this important alert message.
+</div>
+<div class="alert alert-info" role="alert">
+  <strong>Heads up!</strong> This alert needs your attention, but it's not super important.
+</div>
+<div class="alert alert-warning" role="alert">
+  <strong>Warning!</strong> Better check yourself, you're not looking too good.
+</div>
+<div class="alert alert-danger" role="alert">
+  <strong>Oh snap!</strong> Change a few things up and try submitting again.
+</div>
 
-
-                <div class="bs-calltoaction bs-calltoaction-primary">
-                    <div class="row">
-                        <div class="col-md-9 cta-contents">
-                            <h1 class="cta-title h4">Jinrui</h1>
-                            <div class="cta-desc h5">
-                                <p>Hello, wang2 this is a test.Hello, wang2 this is a test. Hello, wang2 this is a test </p>
-                            </div>
-                        </div>
-                        <div class="col-md-3 cta-button">
-                            <a href="#" class="btn btn-lg btn-block btn-primary">Reply</a>
-                        </div>
-                     </div>
+		    <div class="panel-footer clearfix">
+                    <textarea class="form-control" rows="3"></textarea>
+                    <span class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-xs-12" style="margin-top: 10px">
+                        <button class="btn btn-warning btn-lg btn-block" id="btn-chat">Send</button>
+                    </span>
                 </div>
-                <hr>
-                <div class="bs-calltoaction bs-calltoaction-warning">
-                    <div class="row">
-                        <div class="col-md-9 cta-contents">
-                            <h1 class="cta-title h4">Jinrui</h1>
-                            <div class="cta-desc h5">
-                                <p>Test again, how are you?</p>
-                            </div>
-                        </div>
-                        <div class="col-md-3 cta-button">
-                            <a href="#" class="btn btn-lg btn-block btn-warning">Reply</a>
-                        </div>
-                     </div>
-                </div>
-                <hr>
-
+		    
             </div>
         </div>
 
